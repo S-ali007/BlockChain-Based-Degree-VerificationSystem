e1175b1040e8065ac48b258aa9b7f89c4ae8ad98
ali-007 <ali.techtribe007@gmail.com>
2023-12-09 21:37:25 +0500

    Structure in solidity | Struct in Solidity | Loops in solidity | While loop | For loop | Do while loop

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -4,57 +4,198 @@ pragma solidity ^0.8.0;
 
 
 
-contract SimpleStorage {
-    // boolean ,uint .int ,address ,bytes
-
-    uint256 public rollNumber;
-    string public fullName;
-
-    mapping (uint256 => string) public searchByRollNumber;
-
-    struct People {
-        uint256 rollNumber;
-        string fullName;
-    }
-
-    People[] public people;
-
-    function addStudents(uint256 _rollNumber, string memory _name) public {
-        // People memory newPerson = People({
-        //     rollNumber: _rollNumber,
-        //     fullName: _name
-        // });
- 
-        // people.push(newPerson);
-
-        rollNumber = _rollNumber;
-        fullName = _name;
-
-
-    // way2
-         people.push(People(_rollNumber,_name));
-
-
-        // ENV overview ( can access and store  information in six pieces )
-        // calldata ,memory and storage
-
-                searchByRollNumber[_rollNumber] = _name;
-
-    }
-
-    function retrive() public view returns (uint256, string memory) {
-        return (rollNumber, fullName);
-    }
-
-    // function add() public  pure returns  (uint256) {
-    //     return(1+1);
-    // }
-
-    // 0xd9145CCE52D386f254917e481eB44e9943F39138
-}
+import "./SimpleStorage.sol";
 contract StorageFactory{
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public Storage;
+
     function createSimpleStorage() public {
-        simpleStorage=new SimpleStorage();
+        SimpleStorage storage = new SimpleStorage();
+        Storage.push(storage)
+    }
+
+
+
+getter and setter function---------------------------------------------------------------------------------------
+    uint age = 10; state variable--------------
+  
+
+
+    function getter() public view returns(uint){
+        return age;
+
+    }
+
+    function setter(uint newage) public {
+            age=age+1;
+        age=newage;
+    }
+
+
+
+
+  state variable -----------------------------------------------------example2
+    uint public count ;
+
+    constructor in solidity -------------------------------------------------------------------------------------->
+    constructor(){
+
+        count=0;
+
+        // count=new_count ;
+
+
+
+    }
+
+
+    arrays
+    fixed size array and dynamic size array----------------------------------------------------------------------->
+    fixed size array
+    uint[4] public myarra1 =[12,123,11,23];
+
+    function setter(uint index ,uint value) public{
+
+        myarra1[index]=value;
+
+    }
+
+    function length() public view returns (uint){
+
+        return myarra1.length;
+    }
+
+
+
+    Dynamic size array---------------------------------------------------------------------------------------->
+
+
+    uint[] public myarra;
+
+    function pushElement(uint item) public {
+        myarra.push(item);
+    }
+
+    function length() public view returns(uint) {
+        return myarra.length;
+
+    }
+
+
+    function popElement() public  {
+        return myarra.pop();
+    }
+
+    function deleteElement(uint item) public {
+         delete myarra[item];
+    }
+
+    Bytes Array (Fixed Size Array) in solidity ------------------------------------------------------------------>
+
+    bytes3 public b3;
+    bytes4 public b4;
+
+
+    function setter() public {
+       b3='abc';
+     b4='abcd';
+    } 
+
+    bytes[] Array in  (Dynamic Size Array) in solidity-------------------------------------------------------------------->
+
+
+    bytes public dynamicByte="abc";
+
+    function pushElement() public
+    {
+        dynamicByte.push("e");
+    }
+
+    // for access particular index /element
+
+    function getElement(uint i) public  view returns(bytes1) {
+        return dynamicByte[i];
+    }
+
+     function getlength() public view returns(uint){
+        return dynamicByte.length;
+     }   
+
+
+     Loops in solidity | While loop | For loop | Do while loop ------------------------------------------------------------->
+
+    While loop---------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    uint[10] public arr;
+    uint public count;
+
+    function loop() public {
+       while(count<arr.length){
+           arr[count]=count;
+          count++;
+          }
+
+    }
+
+
+ For loop -------------------------------------------------------------------------------------------------------------------->
+    function loop() public{
+
+        for(uint i=count; i<arr.length; i++)
+        {
+            arr[count]=count;
+            count++;
+
+        }
+
+
+
+    }
+     do-while loop -------------------------------------------------------------------------------------------------------------------->
+        function loop() public {
+
+            do{
+                    arr[count]=count;
+                    count++;
+            }
+            while(count<arr.length);
+
+            
+        }
+
+
+    If else in solidity ---------------------------------------------------------------------------------------------------------------->
+    
+    function check(int a) public pure returns(string memory ) 
+    
+    {
+        string memory value;
+
+        if(a>0){
+            value="Greater then 0";
+        }
+
+        else if(a==0) {
+            value="Equal to 0";
+        }
+        else {
+            value="Less then Zero";
+        }
+        return value;
+    }
+
+    Bool Data type in solidity------------------------------------------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    bool public value ;
+    function check(uint a) public returns  (bool){
+
+
+        if(a>100){
+            value=true;
+            return value;
+        }
+        else {
+            value=false;
+            return  value;
+        }
+
     }
 }
